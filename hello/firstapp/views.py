1604c52f5d192f784f5dd5d30c24cab40c4eae88
Evgenii <evgeniikatunkin@gmail.com>
2022-08-29 22:02:07 +0200

    Create paths products and users with parameters

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -3,4 +3,15 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello World!")
+    return HttpResponse("<h2>Main</h2>")
+
+
+def products(request, productid=1):
+    output = "<h2>Product #{0}</h2>".format(productid)
+    return HttpResponse(output)
+
+
+def users(request, id=1, name='Max'):
+    output = "<h2>User</h2><h3>id:{0} " \
+             "Name:{1}</h3>".format(id, name)
+    return HttpResponse(output)
